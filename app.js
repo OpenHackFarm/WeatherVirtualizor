06693272a21d84291fbb01ea93f408ff7b3f0df5
yan <a0726h77@gmail.com>
2018-02-15 00:55:36 +0800

    Add rain and sun chart

--- app.js
+++ app.js
@@ -62,30 +62,54 @@ function drawBarValues() {
 }
 
 function clear_chart() {
-    config.data.datasets.splice(-1, 1);
-    window.myLineChart.update();
+    config_temperature.data.datasets.splice(-1, 1);
+    config_rain.data.datasets.splice(-1, 1);
+    config_sun.data.datasets.splice(-1, 1);
+    window.temperatureLineChart.update();
+    window.rainLineChart.update();
+    window.sunLineChart.update();
 }
 
 function new_chart() {
-    var ctx = document.getElementById("chart").getContext("2d");
-    window.myLineChart = new Chart(ctx, config);
+    var ctx_temperature = document.getElementById('chart_temperature').getContext("2d");
+    var ctx_rain = document.getElementById('chart_rain').getContext("2d");
+    var ctx_sun = document.getElementById('chart_sun').getContext("2d");
+    window.temperatureLineChart = new Chart(ctx_temperature, config_temperature);
+    window.rainLineChart = new Chart(ctx_rain, config_rain);
+    window.sunLineChart = new Chart(ctx_sun, config_sun);
 }
 
-function draw_chart(label, avg_temperature) {
-    if (window.myLineChart === undefined) {
+function draw_chart(label, avg_temperature, avg_rain, avg_sun) {
+    if (window.temperatureLineChart === undefined) {
         new_chart();
     }
 
     var newColor = dynamicColors();
-    config.data.datasets.push({
-        label: label,
+    config_temperature.data.datasets.push({
+        label: label + ' - 氣溫',
         data: avg_temperature,
         fill: false,
         backgroundColor: newColor,
-        borderColor: newColor
+        borderColor: newColor,
+    });
+    config_rain.data.datasets.push({
+        label: label + ' - 降雨量',
+        data: avg_rain,
+        fill: false,
+        backgroundColor: newColor,
+        borderColor: newColor,
+    });
+    config_sun.data.datasets.push({
+        label: label + ' - 日射量',
+        data: avg_sun,
+        fill: false,
+        backgroundColor: newColor,
+        borderColor: newColor,
     });
 
-    window.myLineChart.update();
+    window.temperatureLineChart.update();
+    window.rainLineChart.update();
+    window.sunLineChart.update();
 }
 
 var station_url = "https://raw.githubusercontent.com/OpenHackFarm/CODiS_carwler/master/CWB_Stations_171226.json";
@@ -100,7 +124,7 @@ var config = {
         responsive: true,
         title: {
             display: true,
-            text: '歷史平均溫度統計'
+            text: ''
         },
         tooltips: {
             mode: 'index',
@@ -122,12 +146,9 @@ var config = {
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Temperature'
+                    labelString: ''
                 },
-                ticks: {
-                    min: 8,
-                    max: 32,
-                }
+                ticks: {},
             }]
         },
         animation: {
@@ -136,6 +157,20 @@ var config = {
         },
     }
 };
+var config_temperature = $.extend(true, {}, config);
+config_temperature['options']['title']['text'] = '歷史平均溫度';
+config_temperature['options']['scales']['yAxes'][0]['scaleLabel']['labelString'] = 'Temperature';
+config_temperature['options']['scales']['yAxes'][0]['ticks'] = {min: 8, max: 32};
+
+var config_rain = $.extend(true, {}, config);
+config_rain['options']['title']['text'] = '歷史降雨量統計';
+config_rain['options']['scales']['yAxes'][0]['scaleLabel']['labelString'] = 'Rain';
+config_rain['options']['scales']['yAxes'][0]['ticks'] = {min: 0, max: 900};
+
+var config_sun = $.extend(true, {}, config);
+config_sun['options']['title']['text'] = '歷史日射量統計';
+config_sun['options']['scales']['yAxes'][0]['scaleLabel']['labelString'] = 'Sun';
+config_sun['options']['scales']['yAxes'][0]['ticks'] = {min: 0, max: 700};
 
 $(function() {
     $.getJSON(station_url, function(data) {
@@ -168,7 +203,11 @@ $(function() {
         });
 
         month_temperature = Array.apply(null, Array(12)).map(Number.prototype.valueOf, 0.0);
+        month_rain = Array.apply(null, Array(12)).map(Number.prototype.valueOf, 0.0);
+        month_sun = Array.apply(null, Array(12)).map(Number.prototype.valueOf, 0.00);
         avg_temperature = Array(12);
+        avg_rain = Array(12);
+        avg_sun = Array(12);
         label = $('#station').val().split('_')[1] + ' ' + $('#start_year').val() + '~' + $('#end_year').val();
 
         base_url = 'https://raw.githubusercontent.com/OpenHackFarm/CODiS-data/master/STATION_NAME/YEAR.json'
@@ -190,6 +229,8 @@ $(function() {
                         $.each(data, function(idx, li) {
                             month = idx.split('-')[1];
                             month_temperature[parseInt(month) - 1] += parseFloat(li['Temperature']);
+                            month_rain[parseInt(month) - 1] += parseFloat(li['Precp']);
+                            month_sun[parseInt(month) - 1] += parseFloat(li['GloblRad']);
                         });
                     }
                 }
@@ -201,9 +242,11 @@ $(function() {
         // average temperature
         for (var i = 0; i < 12; i++) {
             avg_temperature[i] = (month_temperature[i] / year_count).toFixed(1);
+            avg_rain[i] = (month_rain[i] / year_count).toFixed(1);
+            avg_sun[i] = (month_sun[i] / year_count).toFixed(2);
         }
 
-        draw_chart(label, avg_temperature);
+        draw_chart(label, avg_temperature, avg_rain, avg_sun);
 
         HoldOn.close();
     });
